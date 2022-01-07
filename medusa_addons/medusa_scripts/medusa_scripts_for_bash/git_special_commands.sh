ef7d89ffdcd2c2d07571109d1cf6878f451e4756
Marcelo Jacinto <marcelo.jacinto@tecnico.ulisboa.pt>
2022-01-07 16:30:01 +0000

    feat: more usefull alias

--- medusa_addons/medusa_scripts/medusa_scripts_for_bash/git_special_commands.sh
+++ medusa_addons/medusa_scripts/medusa_scripts_for_bash/git_special_commands.sh
@@ -1,2 +1,18 @@
 # Pull the latest update in a repo and the corresponding submodules
-alias mgit_pull ='git pull && git submodule update --init --recursive'
+alias mgit_pull='git pull && git submodule update --init --recursive'
+
+# Get the status of the repository and the version of the submodules
+alias mgit_status='git status && git submodule status --recursive'
+
+# ------------------------------------------ #
+# Standard commands, but put as medusa alias #
+# ------------------------------------------ #
+
+# Push the local tags to the remote repository
+alias mgit_push_tag='git push origin --tags'
+
+# Get a general description of the repository
+alias mgit_describe='git describe'
+
+# Get the git log with commits
+alias mgit_log='git log'
